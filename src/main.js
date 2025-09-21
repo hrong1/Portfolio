ab2a5b1fbecc35d7896284fceb342578fa2233b5
hrong1 <hrong1@ucsc.edu>
2025-09-21 15:57:36 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-const appElement = document.getElementById('app');
+const app = document.getElementById('app');
 
 const myInfo = {
   name: 'Haorong Rong',
@@ -44,67 +44,60 @@ contactContainer.append(contactTitle);
 contactContainer.append(contactList);
 app.append(contactContainer);
 
-const portfolioItems = [
-  {
-    title: 'War of the Wilds',
-    imageUrl: 'img/War_of_the_Wilds.png',
-    description: 'A simplistic third-person dungeon crawler with RPG turn-based combat. Players control a small group of animals to explore the dungeon, defeat different enemies, obtain different rewards through treasure chests, and finally reach the end.',
-    link: 'https://store.steampowered.com/app/3689620/War_of_the_Wilds/'
-  },
-  {
-    title: 'Phaser Valley',
-    imageUrl: 'img/phaser_valley.png',
-    description: 'A 2D Phaser game where players can plant different plants. Plants grow based on sun and water levels. When the plants mature, players can reap them to earn money. Players need to earn enough money to reach the game win condition.',
-    link: 'https://hrong1.github.io/CMPM-121-Phaser-Valley/'
-  },
-  {
-    title: 'Sticky Pad',
-    imageUrl: 'img/sticky_pad.png',
-    description: 'Student solo project. A canvas page that allow user draw on. User can change the linewidth and the color of the line. Canvas is also allow user add emoji in the canvas, user can use the emoji that in the web or add new emoji in. Redo, undo, and clear are available for canvas. And it also allow user export the canvas.',
-    link: 'https://hrong1.github.io/cmpm-121-demo-2/'
-  },
-  {
-    title: 'Earn Money',
-    imageUrl: 'img/earn_money.png',
-    description: 'An idle game similar to cookie clicker. Players can press the button in the center to get money, or use the money to buy different things to make the money grow automatically.',
-    link: 'https://hrong1.github.io/cmpm-121-demo-1/'
+
+
+async function loadUserData() {
+  try {
+    const response = await fetch('./data/project.json');
+    if (!response.ok) {
+      throw new Error(`HTTP error, status: ${response.status}`);
+    }
+    const portfolioItems = await response.json();
+    console.log(portfolioItems);
+    displayUserInfo(portfolioItems);
+
+  } catch (error) {
+    console.error('Get data error:', error);
   }
-];
+}
 
-const portfolioTitle = document.createElement('h2');
-portfolioTitle.textContent = 'Portfolio';
-app.append(portfolioTitle);
+function displayUserInfo(portfolioItems) {
+    const portfolioContainer = document.createElement('div');
+    const portfolioTitle = document.createElement('h2');
+    portfolioTitle.textContent = 'Portfolio';
+    app.append(portfolioTitle);
+    portfolioItems.forEach(item => {
+        const card = document.createElement('div');
+        card.className = 'portfolio-card'; 
+        //title
+        const titleElement = document.createElement('h3');
+        titleElement.textContent = item.title;
+        // image
+        const imageElement = document.createElement('img');
+        imageElement.src = item.imageUrl;
+        imageElement.alt = item.description;
+        imageElement.style.width = '600px';
+        imageElement.style.height = 'auto';
+        // text description
+        const descriptionElement = document.createElement('p');
+        descriptionElement.textContent = item.description;
+        descriptionElement.style.maxWidth = '500px';
+        //project link
+        const projectItem = document.createElement('li');
+        const projectLink = document.createElement('a');
+        projectLink.textContent = item.link;
+        projectLink.href = item.link;
+        projectLink.target = item.link;
+        projectItem.append('Project Link: ', projectLink);
+        // add up
+        card.append(titleElement);
+        card.append(imageElement);
+        card.append(descriptionElement);
+        card.append(projectItem)
+        portfolioContainer.append(card);
+    });
+    app.append(portfolioContainer);
+}
 
-const portfolioContainer = document.createElement('div');
-portfolioItems.forEach(item => {
-    const card = document.createElement('div');
-    card.className = 'portfolio-card'; 
-    //title
-    const titleElement = document.createElement('h3');
-    titleElement.textContent = item.title;
-    // image
-    const imageElement = document.createElement('img');
-    imageElement.src = item.imageUrl;
-    imageElement.alt = item.description;
-    imageElement.style.width = '600px';
-    imageElement.style.height = 'auto';
-    // text description
-    const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = item.description;
-    descriptionElement.style.maxWidth = '500px';
-    //project link
-    const projectItem = document.createElement('li');
-    const projectLink = document.createElement('a');
-    projectLink.textContent = item.link;
-    projectLink.href = item.link;
-    projectLink.target = item.link;
-    projectItem.append('Project Link: ', projectLink);
-    // add up
-    card.append(titleElement);
-    card.append(imageElement);
-    card.append(descriptionElement);
-    card.append(projectItem)
-    portfolioContainer.append(card);
-});
+loadUserData();
 
-app.append(portfolioContainer);
